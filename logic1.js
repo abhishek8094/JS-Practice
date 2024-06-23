f719cde452047efdf1d404ea6edd9b243a0a3aeb
abhisek8094 <iasabhi41@gmail.com>
2024-06-23 07:42:16 +0530

    practice

---
+++ logic1.js
@@ -0,0 +1,17 @@
+//check the property exists in object 
+
+var person = {
+    name: "Abhishek",
+    city: "deoria",
+}
+
+// Using in operator
+console.log("city" in person);
+console.log("state" in person); 
+
+//using the hasOwnProperty
+console.log(person.hasOwnProperty("name"));
+console.log(person.hasownProperty("city"));
+
+//comparing the undefined
+console.log(person.city !== undefined);
